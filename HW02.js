b2132bbc776b156f293a15092241a7fe5962ab1c
NapasIsSky <56857844+NapasIsSky@users.noreply.github.com>
2019-10-30 20:06:17 +0700

    Add files via upload

---
+++ HW02.js
@@ -0,0 +1,3 @@
+let array2 = [0,9,1,8,2,7,3,6,4,5];
+array2.sort();
+console.log(array2)
---
+++ HW5.js
@@ -0,0 +1,6 @@
+var array5=[0,9,1,8,2,7,3,6,4,5];
+var i = 0;
+while (i < 10) {
+  array5 += "<br>The number is " + i;
+  i++;
+}
